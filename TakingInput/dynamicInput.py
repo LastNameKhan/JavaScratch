20365a9cdd08779a5398edebd290384c715ef938
LastNameKhan <lastnamekhan47@gmail.com>
2023-08-20 11:36:54 +0530

    Dynamic Input

---
+++ TakingInput/dynamicInput.py
@@ -0,0 +1,6 @@
+#Ask user for there name
+name = input("what is your name?")
+
+#Say hello to the user
+print("Hello,")
+print(name)
---
+++ TakingInput/input.py
@@ -0,0 +1,5 @@
+input("What is your name?")
+print("Hello, David")
+
+# Dynamic taking the input and returning it some
+# value
