b1102a1e3fd7f02f8bf97f64904fb009f635aae0
LastNameKhan <lastnamekhan47@gmail.com>
2023-08-21 09:49:09 +0530

    comments in python

--- TakingInput/dynamicInput.py
+++ TakingInput/dynamicInput.py
@@ -4,3 +4,14 @@ name = input("what is your name?")
 #Say hello to the user
 print("Hello,")
 print(name)
+
+
+#Comments are nice way of writing the pseudocode
+#It helps us to break donw the bigger problems into
+#small points which helps us out to work on it in a 
+#better way
+
+
+#To print it in a single line 
+
+print("Hey, " + name)
