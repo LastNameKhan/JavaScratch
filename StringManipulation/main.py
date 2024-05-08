4d5d4389c5621ba6640d38574a99c7dcd613603b
Aman Khan <lastnamekhan47@gmail.com>
2024-05-08 20:24:49 +0530

    String manipulation

---
+++ StringManipulation/main.py
@@ -0,0 +1,14 @@
+print("Hello World!")
+print("Hello World!")
+print("Hello World!")
+
+#Instead of reapeating it for multiple times we can use back slash n \n
+
+print("Hello World\nHello World\nHello World")
+
+#String Concatenation
+
+#First Method
+print("Hello" + " " + "Aman")
+
+#Second Method
