79f4d1e15f27cd35774316fefe1d21d76e3a3860
Aman Khan <lastnamekhan47@gmail.com>
2024-05-08 20:15:32 +0530

    Console examples practice

---
+++ PrintingConsole/main.py
@@ -0,0 +1,23 @@
+print("Hello World!")
+
+#task Print the given output exactly
+
+print("Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.")
+print("Knead the dough for 10 minutes.")
+print("Add 3g of Salt.")
+print("Leave to rise for 2 hours.")
+print("Bake at 200 degrees C for 30 minutes.")
+
+#task print("She said: "Hello" and then left.")
+
+print('She said: "Hello" and then left.')
+
+#task print("She said: \"Hello\" and then left.")
+#Same can be achieved using the back slash
+
+print("She said: \"Hello\" and then left.")
+
+#Examples 
+print("A 'single quote' inside a double quote")
+print('A "double quote" inside a single quote')
+print("Alternatively you can just \"escape\" the quote")
