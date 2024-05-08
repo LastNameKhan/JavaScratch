3ee1a137426a0e38e8954783f5d46dc9ec48c926
Aman Khan <lastnamekhan47@gmail.com>
2024-05-08 21:10:00 +0530

    Band name generator

---
+++ BandNameGenerator/main.py
@@ -0,0 +1,4 @@
+print("Welcome to Band Name Generator")
+city_name = input("What is the name of yout city?\n")
+pet_name = input("What is the name of your pet?\n")
+print("This is your Band Name is " + city_name + " "+ pet_name)
---
+++ Variables/main.py
@@ -0,0 +1,16 @@
+#To store the input value into an Variable
+
+name = input("What is your name?")
+print(name)
+
+name = "Jack"
+print(name)
+
+# print (len(input("What is your name")))
+#We can writ it in a much simpler manner with the help og variables
+
+name = input('What is your name?')
+length =len(name)
+# print(name)
+# print(length)
+print(name,length)
