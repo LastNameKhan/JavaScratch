0d3a13e2ddbc3616db73bd4359913af486c4c9ba
Aman Khan <lastnamekhan47@gmail.com>
2024-05-08 20:47:12 +0530

    Input Number BODMAS

--- TakingInput/input.py
+++ TakingInput/input.py
@@ -3,3 +3,22 @@ print("Hello, Aman khan")
 
 # Dynamic taking the input and returning it some
 # value added
+
+# input() will get user input in console
+#Then print() will print the word "Hello" and the user input
+print("Hello " + input("What is your name?") + "!")
+
+#Multiple Input Exercise
+print(input())
+print(input())
+print(input())
+
+# print(input("What is your name?"))
+print(input())
+
+#Finding the lenght of the string in python
+numOfLetters = len("Aman")
+print(numOfLetters)
+
+#Finding the lenght of the input value
+print(len(input()))
