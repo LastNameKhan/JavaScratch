3accee6f225b4759bda9b50155e4ed8452670134
Aman Khan <lastnamekhan47@gmail.com>
2024-05-10 13:29:53 +0530

    f strings

---
+++ FStrings/main.py
@@ -0,0 +1,6 @@
+score = 0
+height = 1.8
+isWinniing = True
+
+#f-String
+print(f"Your score is {score} and your height is {height}, your winning status is {isWinniing}")
---
+++ NumberManipulation/main.py
@@ -0,0 +1,26 @@
+#To round the numbers in Python
+
+print(round(8/3,2))
+
+#To print the number when devision without decimal we can use double slash //
+print(8//3)
+print(type(8//3))
+
+#If you want to divide the number futher after first division we can use /=
+
+result = 4/2
+result /=2
+print(result)
+
+score = 0
+
+#Shorhand property for addition
+score = 0
+score += 1
+print(score)
+
+#Shorthand property for subtraction
+limit = 1
+limit -= 1
+print(limit)
+
