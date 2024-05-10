4d0bb7dc951a23224fd28d45566f57f1cfcebd40
Aman Khan <lastnamekhan47@gmail.com>
2024-05-10 11:30:34 +0530

    Data types

---
+++ PrimitiveDataTypes/Integer.py
@@ -0,0 +1,14 @@
+#Integer
+
+#Number Addition
+
+print(123 + 345)
+
+#Float
+
+print(3.14156)
+
+#Boolean
+
+print(True)
+print(False)
---
+++ PrimitiveDataTypes/string.py
@@ -0,0 +1,24 @@
+print(len("Hello"))
+
+# print(len(12345))
+
+# String
+
+#To Print the first value of the String 
+
+print("Hello"[0])
+
+#To print the last value of any given string dynamic input code 
+
+name = "Hello"
+size = len(name)
+print(name[size-1])
+
+#To print the last value hard coded code
+
+print("Hello"[4])
+
+#String Concatination
+
+print("123" + "345")
+
