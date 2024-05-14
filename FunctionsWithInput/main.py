97616a2f4919d1c3401d3b3a347b4f3d8a85c8a0
Aman Khan <lastnamekhan47@gmail.com>
2024-05-14 23:00:01 +0530

    functions with argument

--- FunctionsWithInput/main.py
+++ FunctionsWithInput/main.py
@@ -9,9 +9,16 @@
 
 # Here the name is the parameter and the value inside it is called as an
 # argument Aman is an argument passed by paramter name
-def greet_with_name(name):
-    print(f"Hello {name}")
-    print(f"How do you do {name}?")
-    print(f"Hi {name}")
+# def greet_with_name(name):
+#     print(f"Hello {name}")
+#     print(f"How do you do {name}?")
+#     print(f"Hi {name}")
 
-greet_with_name("Aman")
+# greet_with_name("Aman")
+
+# Functions with more than 1 input
+def greet_with(name,location):
+    print(f"Hello {name}")
+    print(f"What is it like to live in {location}")
+
+greet_with("Aman","Nagda")
