a71684c2bccb39efce9c47b45f75e75f8f05a7b9
Aman Khan <lastnamekhan47@gmail.com>
2024-05-14 23:04:09 +0530

    Multiple arguments

--- FunctionsWithInput/main.py
+++ FunctionsWithInput/main.py
@@ -1,20 +1,19 @@
-# def greet():
-#     print("Hello")
-#     print("How do you do?")
-#     print("Hi")
+def greet():
+    print("Hello")
+    print("How do you do?")
+    print("Hi")
 
-# greet()
+greet()
 
 #Function that allows the input
-
 # Here the name is the parameter and the value inside it is called as an
 # argument Aman is an argument passed by paramter name
-# def greet_with_name(name):
-#     print(f"Hello {name}")
-#     print(f"How do you do {name}?")
-#     print(f"Hi {name}")
+def greet_with_name(name):
+    print(f"Hello {name}")
+    print(f"How do you do {name}?")
+    print(f"Hi {name}")
 
-# greet_with_name("Aman")
+greet_with_name("Aman")
 
 # Functions with more than 1 input
 def greet_with(name,location):
@@ -22,3 +21,10 @@ def greet_with(name,location):
     print(f"What is it like to live in {location}")
 
 greet_with("Aman","Nagda")
+
+# Functiona with keyword
+def greet_with_keyword(name, location):
+    print(f"Hello {name}")
+    print(f"How does it feel like living in {location}")
+
+greet_with_keyword(name="Aman", location="Indore")
