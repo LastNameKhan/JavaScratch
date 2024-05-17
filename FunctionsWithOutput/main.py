4e0ee3923b62b5d5c1fbc9870a48db6dae80226f
Aman Khan <lastnamekhan47@gmail.com>
2024-05-17 23:02:14 +0530

    Functions with Output

---
+++ FunctionsWithOutput/main.py
@@ -0,0 +1,15 @@
+def my_function():
+    result = 3 * 2
+    # We can use the output keyword which is "return" in order to get the output from this function
+    return result
+
+output = my_function()
+print(output)
+
+first_name = input("What is your first name? ")
+last_name = input("What is your Last name? ")
+def format_name(f_name,l_name):
+    return f"{f_name.title()} {l_name.title()}"
+
+formatedName = format_name(first_name,last_name)
+print(formatedName)
