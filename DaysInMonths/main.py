12b803801ea50234bc4f8bb0ba3771f7563fa0c4
Aman Khan <lastnamekhan47@gmail.com>
2024-05-18 00:30:01 +0530

    Debug the issue

--- DaysInMonths/main.py
+++ DaysInMonths/main.py
@@ -1,29 +1,27 @@
-leap_year = False
 def is_leap(year):
   if year % 4 == 0:
     if year % 100 == 0:
       if year % 400 == 0:
-        leap_year = True
+        return True
       else:
-        leap_year = False
+        return False
     else:
-      leap_year = True
+      return True
   else:
-    leap_year = False
+    return False
   
-# TODO: Add more code here 👇
 def days_in_month(year,month):
-  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] 
-  if leap_year == True:
+  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+  if is_leap(year) == True:
     month_days[1] = 29
-    return month_days[month -1]
+    return f"Your Selected Year is {year} which is a Leap Year and days in your selected month is {month_days[month - 1]}"
   else:
     return month_days[month - 1]
   
 
   
-year = int(input()) # Enter a year
-month = int(input()) # Enter a month
+year = int(input("Please Enter the year: \n"))
+month = int(input("Please Enter a month: \n"))
 days = days_in_month(year, month)
 print(days)
 
