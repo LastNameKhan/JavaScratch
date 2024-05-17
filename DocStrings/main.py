22b44d0bff5cd8167d2c65931271ef3a8e57d874
Aman Khan <lastnamekhan47@gmail.com>
2024-05-18 00:40:56 +0530

    DocStrings example

---
+++ DocStrings/main.py
@@ -0,0 +1,7 @@
+def format_name(f_name,l_name):
+    """Take the First and last anem and format it to 
+    return the title case version of the name."""
+    return f_name + " " + l_name
+
+output = format_name(name = "Aman", last = "Khan")
+print(output)
