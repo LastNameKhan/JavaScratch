f081fa276d7bf27b0c3cbab699afb0b723b5947b
Aman Khan <lastnamekhan47@gmail.com>
2024-05-17 23:57:12 +0530

    Months in Days

--- FunctionsWithOutput/main.py
+++ FunctionsWithOutput/main.py
@@ -11,7 +11,7 @@ last_name = input("What is your Last name? ")
 def format_name(f_name,l_name):
     # Multiple Return
     if f_name == "" or l_name == "":
-        return
+        return "You didn't provide valid inputs."
     return f"{f_name.title()} {l_name.title()}"
 
 formatedName = format_name(first_name,last_name)
