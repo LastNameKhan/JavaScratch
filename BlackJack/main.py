5f6c4f850edb446d148b7b5fd0a5e6807db973bb
Aman Khan <lastnamekhan47@gmail.com>
2024-05-19 22:49:13 +0530

    Random Card choice added

--- BlackJack/main.py
+++ BlackJack/main.py
@@ -1,1 +1,15 @@
+import random
+
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+def player_one_card():
+    player_one = random.sample(cards,2)
+    print(player_one)
+
+def computer_card():
+    cpu = random.sample(cards,2)
+    print(cpu)
+
+
+player_one_card()
+computer_card()
