5a4258a8e0859aac7f8bf7f2e1f853c9b354cd06
Aman Khan <lastnamekhan47@gmail.com>
2024-05-20 00:10:14 +0530

    BlackJack Done

--- BlackJack/main.py
+++ BlackJack/main.py
@@ -5,30 +5,39 @@ import random
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
-total_player_one = 0
-player_one = []
-def player_one_card():
-    player_one = random.sample(cards,2)
-    total_player_one = sum(player_one)
-    print(total_player_one)
-    if total_player_one < 21:
-        another_card = input("Would you like to get another card 'y' or 'n' ?").lower()
-        if another_card == "y":
-            random.sample(cards,1).append(player_one)
-            print(player_one)
-    # print(player_one)
-   
+game_start = input("Are you ready to start the Game Type 'y' or 'n': ").lower()
+if game_start == 'y':
+    player_one_card = random.sample(cards,2)
+    player_one_total = sum(player_one_card)
+    cpu_card = random.sample(cards,2)
+    cpu_total = sum(cpu_card)
+    print(player_one_card)
+    print(player_one_total)
+    if player_one_total == 21:
+        print(f"You Win cpu:{cpu_total} you:{player_one_total}")
+    elif cpu_total == 21:
+        print(f"Cpu Wins cpu:{cpu_total} you:{player_one_total}")
+    elif player_one_total == cpu_total:
+        print(f"Snap!! It's an Draw cpu:{cpu_total} you:{player_one_total}")
+    elif player_one_total < 21:
+        plus_one_input = input("Would you like to take another Hit!!!? 'y' or 'n': ").lower()
+        if plus_one_input == 'y':
+            plus_one = random.sample(cards,1)
+            player_one_card.extend(plus_one)
+            plus_one_total = sum(player_one_card)
+            print(player_one_card)
+            print(plus_one_total)
+            print(cpu_card)
+            print(cpu_total)
+            if plus_one_total < cpu_total and cpu_total <= 21:
+                print(f"Cpu Wins cpu:{cpu_total} you:{plus_one_total}")
+            elif plus_one_total > cpu_total and plus_one_total <= 21:
+                print(f"You Win cpu:{cpu_total} you:{plus_one_total}")
+            elif plus_one_total and cpu_total > 21:
+                print(f"No Winner cpu:{cpu_total} you:{plus_one_total}")
+        else:
+            if player_one_total < cpu_total and cpu_total < 21:
+                print(f"Cpu Wins cpu:{cpu_total} you:{player_one_total}")
+            elif player_one_total > cpu_total and player_one_total < 21:
+                print(f"You Win cpu:{cpu_total} you:{player_one_total}")
 
-total_cpu = 0
-cpu = []
-def computer_card():
-    cpu = random.sample(cards,2)
-    total_cpu = sum(cpu)
-    if total_cpu < 21:
-        random.sample(cards,1).append(player_one)
-        print(player_one)
-    print(cpu)
-    print(total_cpu)
-
-player_one_card()
-computer_card()
