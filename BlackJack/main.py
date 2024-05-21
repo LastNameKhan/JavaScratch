db39899ef03314e31c8c154b4803075ee1a94ce0
Aman Khan <lastnamekhan47@gmail.com>
2024-05-21 23:41:42 +0530

    append

--- BlackJack/main.py
+++ BlackJack/main.py
@@ -2,10 +2,17 @@ import random
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
-def deal_cards():
+def deal_card():
+    """Returns a random card from the Deck"""
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
     card = random.choice(cards)
     return card
+
+user_cards = []
+computer_cards = []
+for _ in range(2):
+    new_card = deal_card()
+    user_cards.append(new_card)
 
 
 
