521a23193476ebe44edaab334b6a67b017cf3b95
Aman Khan <lastnamekhan47@gmail.com>
2024-05-21 23:54:15 +0530

    removal from the list

--- BlackJack/main.py
+++ BlackJack/main.py
@@ -11,8 +11,21 @@ def deal_card():
 user_cards = []
 computer_cards = []
 for _ in range(2):
-    new_card = deal_card()
-    user_cards.append(new_card)
+    user_cards.append(deal_card())
+    computer_cards.append(deal_card())
+
+def calculate_score(cards):
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+    
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+
+    return sum(cards)
+
+print(user_cards)
+print(computer_cards)
 
 
 
