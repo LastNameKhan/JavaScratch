4a795968a360a780f57b48b2b9383630604da590
Aman Khan <lastnamekhan47@gmail.com>
2024-05-22 23:01:40 +0530

    Number Guessing Game

---
+++ NumberGuessingGame/main.py
@@ -0,0 +1,9 @@
+import random
+
+print("Welcome to the Number Guessing Game!")
+print("I'm thinking of a number between 1 and 100.")
+print("Choose difficulty level 'easy' or 'hard': ")
+
+# Choose an random number between 1 to 100
+# Give user 10 attempts to guess the number when user choose easy level
+# Give user 5 attempts to guess the number when user chooses hard level
