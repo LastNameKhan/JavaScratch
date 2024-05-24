4d24943046eec71d64f992a00e587e0346efe0a0
Aman Khan <lastnamekhan47@gmail.com>
2024-05-24 19:06:52 +0530

    Questions Added

--- HigherLower/main.py
+++ HigherLower/main.py
@@ -7,8 +7,16 @@ print(logo)
 
 score = 0
 
-compare_a_questions = {"CompareA1":True,"CompareA2":False,"CompareA3":True,"CompareA4":False,"CompareA5":True,"CompareA6":False,"CompareA7":True,"CompareA8":False,"CompareA9":True}
-compare_b_questions = {"CompareB1":True,"CompareB2":False,"CompareB3":True,"CompareB4":False,"CompareB5":True,"CompareB6":False,"CompareB7":True,"CompareB8":False,"CompareB9":True}
+compare_a_questions = {"Does the population of New York City exceed 8 million?":True,
+                       "Does Cristiano Ronaldo have more than 200 million Instagram followers?":False,
+                       "Is Jeff Bezos' net worth higher than $150 billion?":True,
+                       "Does 'The Shawshank Redemption' have an IMDb rating higher than 9.0?":False,
+                       "CompareA5":True,
+                       "CompareA6":False,"CompareA7":True,"CompareA8":False,"CompareA9":True}
+compare_b_questions = {"Does the population of Los Angeles exceed 4 million?":True,
+                       "Does Kim Kardashian have more than 200 million Instagram followers?":False,
+                       "Is Elon Musk's net worth higher than $150 billion?":True,
+                       "Does 'The Godfather' have an IMDb rating higher than 9.0?":False,"CompareB5":True,"CompareB6":False,"CompareB7":True,"CompareB8":False,"CompareB9":True}
 
 
 continuation = True
