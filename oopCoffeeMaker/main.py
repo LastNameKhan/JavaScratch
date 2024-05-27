c7d3d953ad86db161be47799957476da82af19c6
Aman Khan <lastnamekhan47@gmail.com>
2024-05-27 23:37:39 +0530

    Methods Added

--- oopCoffeeMaker/main.py
+++ oopCoffeeMaker/main.py
@@ -1,3 +1,8 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+my_money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+print(my_money_machine.report())
+print(coffee_maker.report())
