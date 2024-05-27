b0260f4a4f45bd9db3166e5c1a245ed76c199295
Aman Khan <lastnamekhan47@gmail.com>
2024-05-27 23:44:05 +0530

    Payment done

--- oopCoffeeMaker/main.py
+++ oopCoffeeMaker/main.py
@@ -4,5 +4,19 @@ from money_machine import MoneyMachine
 
 my_money_machine = MoneyMachine()
 coffee_maker = CoffeeMaker()
-print(my_money_machine.report())
-print(coffee_maker.report())
+menu = Menu()
+is_on = True
+
+
+
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like to have {options}")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        my_money_machine.report()
+        coffee_maker.report()
+    else:
+        drink = menu.find_drink(choice)
+        print(coffee_maker.is_resource_sufficient(drink))
