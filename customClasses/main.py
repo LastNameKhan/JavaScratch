b5621e02d4542b5861741625739cdfc4b65ad916
Aman Khan <lastnamekhan47@gmail.com>
2024-05-29 20:10:43 +0530

    added functions

--- customClasses/main.py
+++ customClasses/main.py
@@ -1,7 +1,24 @@
-# Creating class using the Class Keyword
 class User:
+    def __int__(self, user_id):
+        self.id = user_id
+        print("New user being created")
 
 
+ def __int__(self, user_id):
+        self.id = user_id
+        print("New user being created")
 
-# Getting the Class
+        def __int__(self, user_id):
+            self.id = user_id
+            print("New user being created")
 user_1 = User()
+user_1.id = '001'
+user_1.username = "Aman"
+
+print(user_1.username)
+
+user_2 = User()
+user_2.id = "002"
+user_2.username = "Jack"
+
+print(user_2.username)
