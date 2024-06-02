9d9d0660146cb2dc30977002bfbe26a22a627084
Aman Khan <lastnamekhan47@gmail.com>
2024-06-02 22:17:33 +0530

    Code Flow

--- QuizGame/quiz_brain.py
+++ QuizGame/quiz_brain.py
@@ -5,15 +5,18 @@ class QuizBrain:
         self.score = 0
 
     def still_has_question(self):
+        """Continue Asking Questions"""
         return self.question_number < len(self.question_list)
 
     def next_question(self):
+        """Next Question on Demand"""
         current_question = self.question_list[self.question_number]
         self.question_number += 1
         user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False)")
         self.check_answer(user_answer, current_question.answer)
 
     def check_answer(self, user_answer, correct_answer):
+        """Check for the answer """
         if user_answer.lower() == correct_answer.lower():
             print("You got it right")
             self.score += 1
