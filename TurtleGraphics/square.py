fd67b47e8371739bf0b6ab1a4d480af78007f639
Aman Khan <lastnamekhan47@gmail.com>
2024-06-05 23:45:02 +0530

    added packages

--- TurtleGraphics/square.py
+++ TurtleGraphics/square.py
@@ -1,10 +1,19 @@
 from turtle import Turtle, Screen
 
 little_turtle = Turtle()
+tri_turtle = Turtle()
+tri_turtle.forward(200)
 
+# Square
 for i in range(4):
     little_turtle.forward(100)
     little_turtle.right(90)
+
+# Trapezium
+for i in range(6):
+    tri_turtle.forward(100)
+    tri_turtle.right(60)
+
 
 
 
