33c0b7a67724f7ac2d295c2844157ca885e2d88f
Aman Khan <lastnamekhan47@gmail.com>
2024-06-05 23:56:18 +0530

    dashed line done

--- TurtleGraphics/dashed.py
+++ TurtleGraphics/dashed.py
@@ -2,7 +2,11 @@ from turtle import Turtle, Screen
 
 rick_turtle = Turtle()
 
-
+for _ in range(15):
+    rick_turtle.forward(10)
+    rick_turtle.penup()
+    rick_turtle.forward(10)
+    rick_turtle.pendown()
 
 
 
