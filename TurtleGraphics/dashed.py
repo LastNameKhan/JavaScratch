13e6552ead8745efab2713098d06e77ca6e14802
Aman Khan <lastnamekhan47@gmail.com>
2024-06-05 23:58:31 +0530

    for loop correction

--- TurtleGraphics/dashed.py
+++ TurtleGraphics/dashed.py
@@ -2,7 +2,7 @@ from turtle import Turtle, Screen
 
 rick_turtle = Turtle()
 
-for _ in range(15):
+for i in range(15):
     rick_turtle.forward(10)
     rick_turtle.penup()
     rick_turtle.forward(10)
