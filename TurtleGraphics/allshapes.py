9f13c8b17a3018bbd0c3ace215489b66e60bd97a
Aman Khan <lastnamekhan47@gmail.com>
2024-06-11 18:14:02 +0530

    multiple shapes

--- TurtleGraphics/allshapes.py
+++ TurtleGraphics/allshapes.py
@@ -1,8 +1,10 @@
 from turtle import Turtle, Screen
 
 runner = Turtle()
+screen = Screen()
 number_of_angles = 3
 
+screen.setup(width=800, height=600, startx=200, starty=200)
 
 for i in range(number_of_angles):
     angle = 360/ number_of_angles
@@ -22,31 +24,37 @@ def triangle():
         runner.right(120)
 
 def pentagon():
+    """To Print the Pentagon"""
     for i in range(5):
         runner.forward(100)
         runner.right(72)
 
 def hexagon():
+    """To Print the hexagon"""
     for i in range(6):
         runner.forward(100)
         runner.right(60)
 
 def heptagon():
+    """To Print the heptagon"""
     for i in range(7):
         runner.forward(100)
         runner.right(51.5)
 
 def octagon():
+    """To Print the octagon"""
     for i in range(8):
         runner.forward(100)
         runner.right(45)
 
 def nonagon():
+    """To Print the nonagon"""
     for i in range(9):
         runner.forward(100)
         runner.right(40)
 
 def decagon():
+    """To Print the decagon"""
     for i in range(10):
         runner.forward(100)
         runner.right(36)
