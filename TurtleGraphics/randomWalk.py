f00976653d288fe2a7ad0c01dfac067cfa3765ee
Aman Khan <lastnamekhan47@gmail.com>
2024-06-13 16:12:54 +0530

    immutable tuples

--- TurtleGraphics/randomWalk.py
+++ TurtleGraphics/randomWalk.py
@@ -1,17 +1,27 @@
+import turtle
 from turtle import Turtle, Screen
 import random
 
 walking = Turtle()
-colours = ["black", "red", "yellow", "blue", "teal", "green", "purple", "wheat", "DeepSkyBlue"]
+turtle.colormode(255)
+
+def randomColor():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_color = (r, g, b)
+    return random_color
+
+
 direction = [0, 90, 180, 270]
 distance = random.randint(0, 100)
 walking.pensize(5)
 walking.speed("fastest")
 
-for i in range(200):
+for i in range(1000):
     walking.forward(30)
     walking.setheading(random.choice(direction))
-    walking.color(random.choice(colours))
+    walking.color(randomColor())
 
 
 screen = Screen()
