a94d0edaec5a2ab86a8c5298fc1c816a07f0caed
Aman Khan <lastnamekhan47@gmail.com>
2024-06-13 17:42:01 +0530

    setting the start of turtle

--- hirstPainting/main.py
+++ hirstPainting/main.py
@@ -1,14 +1,18 @@
+import turtle
+import random
+
 import colorgram
+from turtle import Turtle, Screen
 
-colors = colorgram.extract('hirst.PNG', 30)
-rgb_colors = []
-for color in colors:
-    r = color.rgb.r
-    g = color.rgb.g
-    b = color.rgb.b
-    rgb_colors.append((r, g, b))
-
-print(rgb_colors)
+# colors = colorgram.extract('hirst.PNG', 30)
+# rgb_colors = []
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     rgb_colors.append((r, g, b))
+#
+# print(rgb_colors)
 
 
 color_list = [(250, 248, 245), (251, 246, 249), (242, 249, 246), (241, 246, 250),
@@ -20,3 +24,18 @@ color_list = [(250, 248, 245), (251, 246, 249), (242, 249, 246), (241, 246, 250)
  (114, 101, 203), (189, 15, 3), (121, 216, 234), (156, 215, 200),
  (66, 109, 97), (10, 96, 69)]
 
+turtle.colormode(255)
+timmy = Turtle()
+timmy.setheading(225)
+timmy.forward(250)
+timmy.setheading(0)
+
+for _ in range(10):
+    timmy.dot(10, random.choice(color_list))
+    timmy.penup()
+    timmy.forward(50)
+
+
+
+screen = Screen()
+screen.exitonclick()
