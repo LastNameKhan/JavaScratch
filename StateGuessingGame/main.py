c9d10a95854e2e353d4b055f7a678fcc2685f81b
Aman Khan <lastnamekhan47@gmail.com>
2024-07-02 19:09:27 +0530

    State guessing completed

--- StateGuessingGame/main.py
+++ StateGuessingGame/main.py
@@ -10,17 +10,34 @@ turtle.shape(image)
 
 game_is_on = False
 count = 0
+guessed_state = []
+
 while not game_is_on:
     answer_state = screen.textinput(title=f"Guess the State {count}/50", prompt="What's another state's name?")
     data = pandas.read_csv("50_states.csv")
     state_name = data["state"].tolist()
+    if answer_state == "exit":
+        missing_states = []
+        for state in state_name:
+            if state not in guessed_state:
+                missing_states.append(state)
+        data_to_csv = {
+            "missing_state" : missing_states
+        }
+        created_file = pandas.DataFrame(data_to_csv)
+        created_csv = created_file.to_csv("missing_state")
+        print(created_csv)
     if answer_state in state_name:
+        guessed_state.append(answer_state)
         count += 1
         state_turtle = turtle.Turtle()
         state_turtle.hideturtle()
         fetched_row_data = data[data["state"] == answer_state]
         x_cor = fetched_row_data["x"].values[0]
         y_cor = fetched_row_data["y"].values[0]
+        # Another method for the above code is
+        # x_cor = int(fetched_row_data.x)
+        # y_cor = int(fetched_row_data.y)
         state_turtle.penup()
         state_turtle.goto(x_cor, y_cor)
         state_turtle.pendown()
@@ -32,6 +49,7 @@ while not game_is_on:
         game_is_on = True
 
 
+
 # Below function was used to get the coordinaltion of the mouse click event x and y coordinates
 # def get_mouse_click_coor(x, y):
 #     print(x, y)
@@ -39,5 +57,5 @@ while not game_is_on:
 #
 # turtle.onscreenclick(get_mouse_click_coor)
 
-turtle.mainloop()
+# turtle.mainloop()
 
