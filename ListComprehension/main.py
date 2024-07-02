5177a85d589372e25ca5ce5516c8f36081da4fc1
Aman Khan <lastnamekhan47@gmail.com>
2024-07-03 01:27:57 +0530

    List Comprehension

---
+++ ListComprehension/main.py
@@ -0,0 +1,19 @@
+# Comprehension is the used to create a list from the existing list without using the for loop
+
+numbers = [1, 2, 3]
+# new_list = [new_item for item in list]
+# Below is the syntax for the list Comprehension
+new_list = [item+1 for item in numbers]
+print(new_list)
+
+
+# To do operation in string using List Compreshension
+
+name = "AmanKhan"
+new_string = [n for n in name]
+# The above code will return us the Each alphabet in the list
+print(new_string)
+
+# Working with the range in List Comprehension
+new_range_data = [n for n in range(1, 5)]
+print(new_range_data)
